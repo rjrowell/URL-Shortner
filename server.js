cca04b96290d9a748706a68cb88ec6002a0947d1
rjrowell <106752494+rjrowell@users.noreply.github.com>
2022-12-06 13:31:01 +0000

    now working as intended

--- server.js
+++ server.js
@@ -14,13 +14,28 @@ mongoose.connect('mongodb://127.0.0.1:27017/urlShortener',{
 app.set('view engine', 'ejs')
 //.use is for middleware
 app.use(express.urlencoded({extended: false}))
-app.get('/',(req,resp)=>{
-    resp.render('index')
+
+//route for index file
+app.get('/',async (req,resp)=>{
+    const shortUrls = await shortUrl.find()
+    resp.render('index',{shortUrls: shortUrls})
 })
 
+//route for creating short url and adding to db
 app.post('/shortUrls',async (req,resp)=>{
     await shortUrl.create({full: req.body.longUrl})
     resp.redirect('/')
+})
+
+//route for the shortUrl
+app.get('/:shortUrl',async (req,resp)=>{
+    const newUrl = await shortUrl.findOne({
+        short:req.params.shortUrl
+    })
+    if (newUrl == null){
+        resp.sendStatus(404)
+    }
+    resp.redirect(newUrl.full)
 })
 
 app.listen(process.env.PORT || 5000);
